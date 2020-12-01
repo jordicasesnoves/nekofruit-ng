919872c8d13f8e7d54811b8bb165b2e95b73a44b
Jordi Casesnoves <jordicasesnoves@gmail.com>
2020-12-01 10:30:19 +0100

    addItemToCart() now can modify quantity property

--- src/app/shared/components/product-grid/product-grid.component.ts
+++ src/app/shared/components/product-grid/product-grid.component.ts
@@ -37,6 +37,9 @@ export class ProductGridComponent implements OnInit {
       error(msg) {
         console.log(msg);
       },
+      complete() {
+        console.log('complete triggered');
+      },
     });
   }
 }
