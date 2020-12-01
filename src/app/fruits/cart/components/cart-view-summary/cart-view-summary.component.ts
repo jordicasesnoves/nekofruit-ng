50069030fcc9ffbb501c83fa2540009464c53d45
Jordi Casesnoves <jordicasesnoves@gmail.com>
2020-12-01 15:49:05 +0100

    added cart total

--- src/app/fruits/cart/components/cart-view-summary/cart-view-summary.component.ts
+++ src/app/fruits/cart/components/cart-view-summary/cart-view-summary.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
+
 import { CartItem } from 'src/app/shared/models/backendModels';
 
 @Component({
@@ -6,10 +14,26 @@ import { CartItem } from 'src/app/shared/models/backendModels';
   templateUrl: './cart-view-summary.component.html',
   styleUrls: ['./cart-view-summary.component.scss'],
 })
-export class CartViewSummaryComponent implements OnInit {
+export class CartViewSummaryComponent implements OnInit, OnChanges {
   @Input() cartItems: CartItem[];
+  loading: boolean = true;
+  total: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.cartItems.currentValue && this.loading) {
+      this.calculateTotal();
+      this.loading = false;
+    }
+  }
+
+  calculateTotal(): void {
+    this.cartItems.forEach((item) => {
+      this.total = this.total + item.product.price_EUR * item.quantity;
+    });
+    this.total = Math.round(this.total * 100) / 100;
+  }
 }
